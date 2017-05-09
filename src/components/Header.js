bde8025ba176caa3157c67dd6ebdd8eae28d9885
Jeffrey Lembeck <jlembeck@gmail.com>
2017-05-08 20:42:32 -0700

    feat(lecture): header link + protypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 import './Header.css'
 
@@ -6,8 +7,10 @@ const Header = (props) => {
   return (
     <header>
       <h1>
-        <span className='car-word'>Pauline's</span>
-        <span className='cdr-word'>Perfect Patisserie</span>
+        <Link to='/'>
+          <span className='car-word'>Pauline's</span>
+          <span className='cdr-word'>Perfect Patisserie</span>
+        </Link>
       </h1>
     </header>
   )
--- src/components/PastryList.js
+++ src/components/PastryList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import './PastryList.css'
 
@@ -17,6 +18,10 @@ class PastryList extends React.Component {
       </ul>
     )
   }
+}
+
+PastryList.propTypes = {
+  pastries: PropTypes.object.isRequired
 }
 
 export default PastryList
